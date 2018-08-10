e1cfe5ba2fbe5f8e8d1a0daa953bd2d82a9ceb8f
Roland <rdorde@gmail.com>
2018-08-10 19:00:07 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+function headingMouseOverHandler(e) {
+    e.target.textContent = 'VEGANIT';
+}
+function headingMouseOutHandler(e) {
+    e.target.textContent = 'VEGANYT';
+}
 
+document.addEventListener('DOMContentLoaded', function () {
+    let headingWords = document.querySelectorAll('.vy-heading span');
+    headingWords.forEach(word => word.addEventListener('mouseover', headingMouseOverHandler));
+    headingWords.forEach(word => word.addEventListener('mouseout', headingMouseOutHandler));
+});
