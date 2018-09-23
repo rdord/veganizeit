8a4eb999135cf8158c5dc3154a023d35f234688a
Roland Đorđević <roland.djordjevic@ineor.si>
2018-09-24 01:03:06 +0200

    refactor

--- js/main.js
+++ js/main.js
@@ -1,54 +1,53 @@
 function headingMouseOverHandler(e) {
     e.target.textContent = 'VEGANIT';
 }
+
 function headingMouseOutHandler(e) {
     e.target.textContent = 'VEGANYT';
 }
 
-function getRandomVidNumber() {
-    fetch('https://www.googleapis.com/youtube/v3/playlistItems?part=id&maxResults=0&playlistId=PLszeRFrbboBASL8JEvt_jztJyXaXHMf8-&key=AIzaSyDk8gwNe0f5Ue7uls-ir8U1Mj7UAjJvMeo')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        let totalVids = JSON.stringify(data.pageInfo.totalResults)
-        let videoIndex = Math.floor(Math.random() * totalVids);
-        var player;
-        var isUnMuted = false;
-
-        window.onYouTubeIframeAPIReady = function() {
-            player = new YT.Player('player', {
-                playerVars: {
-                    'width': 853,
-                    'height': 480,
-                    'list': 'PLszeRFrbboBASL8JEvt_jztJyXaXHMf8-',
-                    'listType': 'playlist',
-                    'rel': 0,
-                    'index': videoIndex,
-                    'modestbranding': 1,
-                    'origin': 'http://veganyt.com'
-                },
-                events: {
-                    'onReady': function() {
-                        player.playVideo();
-                        player.setVolume(100);
-                    }
-                }
-            });
-        }
-    });
+async function getRandomVidNumber() {
+    const response = await fetch('https://www.googleapis.com/youtube/v3/playlistItems?part=id&maxResults=0&playlistId=PLszeRFrbboBASL8JEvt_jztJyXaXHMf8-&key=AIzaSyDk8gwNe0f5Ue7uls-ir8U1Mj7UAjJvMeo', {});
+    const data = await response.json();
+    const totalVids = JSON.stringify(data.pageInfo.totalResults)
+    const videoIndex = Math.floor(Math.random() * totalVids);
+    return videoIndex;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    let headingWords = document.querySelectorAll('.vy-heading span');
+var player;
+
+async function onYouTubeIframeAPIReady() {
+    const videoIndex = await getRandomVidNumber();
+    player = new YT.Player('vy-video__embed', {
+        height: '480',
+        width: '853',
+        playerVars: {
+            'list': 'PLszeRFrbboBASL8JEvt_jztJyXaXHMf8-',
+            'listType': 'playlist',
+            'rel': 0,
+            'index': videoIndex,
+            'modestbranding': 1,
+            'origin': 'http://veganyt.com',
+            'loop': 1
+            // 'showinfo': 0
+        },
+        events: {
+            'onReady': () => {
+                player.playVideo();
+                player.setVolume(100);
+                console.log('videoID', player.getPlaylist()[0])
+                setTimeout(() => player.setShuffle(true), 1000);
+            }
+        }
+    });
+
+    
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const headingWords = document.querySelectorAll('.vy-heading span');
     headingWords.forEach(word => word.addEventListener('mouseover', headingMouseOverHandler));
-    headingWords.forEach(word => word.addEventListener('mouseout', headingMouseOutHandler));    
+    headingWords.forEach(word => word.addEventListener('mouseout', headingMouseOutHandler));
+
+    document.getElementById('vy-next-video').onclick = () => player.nextVideo();
 });
-
-
-var tag = document.createElement('script');
-tag.src = "//www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-getRandomVidNumber();
-
